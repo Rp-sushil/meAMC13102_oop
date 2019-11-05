bc5957541ec604f23486a749a85694fd4ca1d37c
SUSHIL KUMAR <sushil.18je0861@am.iitism.ac.in>
2019-11-05 12:29:24 +0530

    Conditional statement that prints “It’s true”.
    Write a conditional statement that prints “It’s true”.

---
+++ labmanual/exp_13.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String s = new String();
+		if(3 < 4){
+		    s = "It's true";
+		    System.out.println(s);
+		}else{
+		    s = "It's false";
+		    System.out.println(s);
+		}
+		
+	}
+}
